1406737403526d8dd4c13d94f9591379fc7ea906
Jacob Fliss <jake@newagesoldier.com>
2015-08-27 23:17:55 -0400

    Added a trainer manager program

---
+++ Trainer Manager/Trainer Manager/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace Trainer_Manager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.backgroundWorker2 = new System.ComponentModel.BackgroundWorker();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.dlprogressLabel = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(0, 0);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(353, 514);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listBox1_MouseDoubleClick);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(7, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(345, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(6, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(102, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Open Trainer";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // backgroundWorker2
+            // 
+            this.backgroundWorker2.WorkerReportsProgress = true;
+            this.backgroundWorker2.WorkerSupportsCancellation = true;
+            this.backgroundWorker2.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker2_DoWork);
+            this.backgroundWorker2.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker2_ProgressChanged);
+            this.backgroundWorker2.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(114, 0);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(325, 23);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // dlprogressLabel
+            // 
+            this.dlprogressLabel.AutoSize = true;
+            this.dlprogressLabel.Location = new System.Drawing.Point(254, 5);
+            this.dlprogressLabel.Name = "dlprogressLabel";
+            this.dlprogressLabel.Size = new System.Drawing.Size(35, 13);
+            this.dlprogressLabel.TabIndex = 4;
+            this.dlprogressLabel.Text = "label1";
+            this.dlprogressLabel.Visible = false;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.dlprogressLabel);
+            this.panel1.Controls.Add(this.progressBar1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(353, 484);
+            this.panel1.Margin = new System.Windows.Forms.Padding(15, 3, 3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Padding = new System.Windows.Forms.Padding(15, 0, 0, 0);
+            this.panel1.Size = new System.Drawing.Size(442, 30);
+            this.panel1.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(358, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(57, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "browse";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.button3);
+            this.panel2.Controls.Add(this.textBox1);
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(353, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(442, 30);
+            this.panel2.TabIndex = 7;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(417, 3);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(20, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "X";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(795, 514);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.listBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Cheat Trainer Manager :: newagesoldier.com";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.form1_Closing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label dlprogressLabel;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button3;
+
+    }
+}
+
---
+++ Trainer Manager/Trainer Manager/Form1.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.IO;
+using System.Net;
+using System.Web;
+using Ionic.Zip;
+
+namespace Trainer_Manager
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string defaultDir = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\cheat_trainers";
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.Text = Properties.Settings.Default.trainer_folder;
+            if (!backgroundWorker1.IsBusy)
+                backgroundWorker1.RunWorkerAsync();
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            listBox1.Invoke(new MethodInvoker(delegate { listBox1.Items.Clear(); }));
+
+            XmlTextReader reader = new XmlTextReader("https://newagesoldier.com/myfiles/trainers/tscan.php");
+            while (reader.Read()) //read line by line
+            {
+                try
+                {
+                    if (reader.Name == "name")
+                        listBox1.Invoke(new MethodInvoker(delegate { listBox1.Items.Add(reader.ReadString()); }));
+
+                    //if (reader.Name == "title")
+                    //    title = reader.ReadString();
+                }
+                catch
+                {
+                    //MessageBox.Show("Could not create covers. Your internet connection may have been interrupted.");
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!Directory.Exists(textBox1.Text))
+                Directory.CreateDirectory(textBox1.Text);
+
+            string[] dirs = Directory.GetFiles(textBox1.Text, listBox1.Text + @".*");
+
+            if (dirs == null || dirs.Length == 0) //begin the download
+            {
+                if (!backgroundWorker2.IsBusy)
+                    backgroundWorker2.RunWorkerAsync();
+            }
+        }
+
+        static String BytesToString(double byteCount)
+        {
+            string[] suf = { "B", "KB", "MB", "GB", "TB", "PB", "EB" }; //Longs run out around EB
+            if (byteCount == 0)
+                return "0" + suf[0];
+            long bytes = Math.Abs((long)byteCount);
+            int place = Convert.ToInt32(Math.Floor(Math.Log(bytes, 1024)));
+            double num = Math.Round(bytes / Math.Pow(1024, place), 1);
+            return (Math.Sign(byteCount) * num).ToString() + suf[place];
+        }
+
+        public void ExtractFileToDirectory(string zipFileName, string outputDirectory)
+        {
+            dlprogressLabel.Invoke(new MethodInvoker(delegate { dlprogressLabel.Text = "Unzipping files..."; }));
+
+            FileStream fs = File.OpenRead(zipFileName);
+            string tmpFile = "";
+            if (zipFileName.Contains(".zip") == true)
+            {
+                ZipFile zip = ZipFile.Read(fs);
+                foreach (ZipEntry e in zip)
+                {
+                    e.Extract(outputDirectory, ExtractExistingFileAction.OverwriteSilently);
+                }
+                tmpFile = outputDirectory + @"\tmp.zip";
+            }
+
+            fs.Close();
+
+            File.Delete(tmpFile);
+            dlprogressLabel.Visible = false;
+            backgroundWorker2.CancelAsync();
+        }
+
+        private void backgroundWorker2_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+            if (dlprogressLabel.InvokeRequired)
+                dlprogressLabel.Invoke(new MethodInvoker(delegate
+                {
+                    dlprogressLabel.Text = e.ProgressPercentage.ToString() + "%";
+                }));
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            foreach (var file in Directory.GetFiles(textBox1.Text + "\\" + listBox1.Text, "*.exe", SearchOption.AllDirectories))
+            {
+                System.Diagnostics.Process.Start(file);
+            }
+        }
+
+        void listBox1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            button1.PerformClick();
+        }
+
+        private void backgroundWorker2_DoWork(object sender, DoWorkEventArgs e)
+        {
+            string realURL = null;
+            string sFilePathToWriteFileTo = null;
+
+            listBox1.Invoke(new MethodInvoker(delegate
+            {
+                realURL = HttpUtility.HtmlDecode("https://newagesoldier.com/myfiles/trainers/" + listBox1.Text + ".zip");
+                if (!Directory.Exists(textBox1.Text + "\\" + listBox1.Text))
+                    Directory.CreateDirectory(textBox1.Text + "\\" + listBox1.Text);
+                sFilePathToWriteFileTo = textBox1.Text + "\\" + listBox1.Text + @"\tmp.zip";
+            }));
+
+            string sUrlToReadFileFrom = realURL;
+
+            Uri url = new Uri(sUrlToReadFileFrom);
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            response.Close();
+            long iSize = response.ContentLength;
+            Int64 iRunningByteTotal = 0;
+
+            dlprogressLabel.Invoke(new MethodInvoker(delegate
+            {
+                dlprogressLabel.Visible = true;
+            }));
+
+            using (WebClient DLclient = new WebClient())
+            {
+                using (Stream streamRemote = DLclient.OpenRead(new Uri(sUrlToReadFileFrom)))
+                {
+                    using (Stream streamLocal = new FileStream(sFilePathToWriteFileTo, FileMode.Create, FileAccess.Write, FileShare.None))
+                    {
+                        int iByteSize = 0;
+                        byte[] byteBuffer;
+                        byteBuffer = new byte[iSize];
+                        while ((iByteSize = streamRemote.Read(byteBuffer, 0, byteBuffer.Length)) > 0)
+                        {
+                            streamLocal.Write(byteBuffer, 0, iByteSize);
+                            iRunningByteTotal += iByteSize;
+
+                            if (backgroundWorker2.CancellationPending == true)
+                            {
+                                e.Cancel = true;
+                                break;
+                            }
+
+                            double dIndex = (double)(iRunningByteTotal);
+                            double dTotal = (double)byteBuffer.Length;
+                            double dProgressPercentage = (dIndex / dTotal);
+                            int iProgressPercentage = (int)(dProgressPercentage * 100);
+                            if (dIndex > 0 && dTotal > 0)
+                            {
+                                    dlprogressLabel.Invoke(new MethodInvoker(delegate
+                                    {
+                                        dlprogressLabel.Text = BytesToString(dIndex).ToString() + "/" + BytesToString(dTotal).ToString() + " (" + iProgressPercentage.ToString() + "%)";
+                                    }));
+                            }
+                            backgroundWorker2.ReportProgress(iProgressPercentage);
+                        }
+                        streamLocal.Close();
+                    }
+                    streamRemote.Close();
+                }
+            }
+            listBox1.Invoke(new MethodInvoker(delegate
+            {
+                ExtractFileToDirectory(sFilePathToWriteFileTo, textBox1.Text + "\\" + listBox1.Text);
+            }));
+        }
+
+        private void form1_Closing(object sender, FormClosingEventArgs e)
+        {
+            Properties.Settings.Default.trainer_folder = textBox1.Text;
+            Properties.Settings.Default.Save();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            DialogResult result = fbd.ShowDialog();
+
+            string[] files = Directory.GetFiles(fbd.SelectedPath);
+            textBox1.Text = fbd.SelectedPath;
+        }
+
+        private void form1_Closing()
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = defaultDir;
+        }
+    }
+}
