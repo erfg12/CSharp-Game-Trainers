bfae31bed39bc33377f79e2eadac15937530c449
Jacob Fliss <jake@newagesoldier.com>
2016-01-15 20:20:12 -0500

    updated UI, fixed a small crash bug

--- Trainer Manager/Trainer Installer/codesign.bat
+++ Trainer Manager/Trainer Installer/codesign.bat
@@ -1,5 +1,5 @@
 @echo off
 set /p id="Enter private key file pass: "
-"C:\Program Files (x86)\Windows Kits\10\bin\x86\signtool" sign /t http://timestamp.verisign.com/scripts/timestamp.dll /f "C:\Users\Jake.CUTTING\Documents\My Private Key.pfx" /p %id% "Release\setup.exe"
-"C:\Program Files (x86)\Windows Kits\10\bin\x86\signtool" sign /t http://timestamp.verisign.com/scripts/timestamp.dll /f "C:\Users\Jake.CUTTING\Documents\My Private Key.pfx" /p %id% "Release\Trainer Installer.msi"
+"C:\Program Files (x86)\Windows Kits\10\bin\x86\signtool" sign /t http://timestamp.verisign.com/scripts/timestamp.dll /f "C:\Users\web\Documents\My Private Key.pfx" /p %id% "Release\setup.exe"
+"C:\Program Files (x86)\Windows Kits\10\bin\x86\signtool" sign /t http://timestamp.verisign.com/scripts/timestamp.dll /f "C:\Users\web\Documents\My Private Key.pfx" /p %id% "Release\Trainer Installer.msi"
 pause
