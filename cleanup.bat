90f8a24df9882942baa2fa47191cc5fb505014c6
erfg12 <jake@newagesoldier.com>
2016-05-03 07:31:03 -0400

    clean up

--- cleanup.bat
+++ cleanup.bat
@@ -1,6 +1,3 @@
-for /d /r . %%d in (obj,debug,.vs,bin) do @if exist "%%d" rd /s/q "%%d"
+for /d /r . %%d in (obj,.vs,bin) do @if exist "%%d" rd /s/q "%%d"
 del /S /F /AH *.suo
 del /S /F /AH *.htm
-del /S /F /AH *.exe
-rmdir /S /F /AH Debug
-rmdir /S /F /AH Release
