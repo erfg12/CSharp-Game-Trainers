a297da8b8ba525f42590b77b2f709b89bf530d49
erfg12 <jake@newagesoldier.com>
2016-07-12 15:24:52 -0400

    CoD MW 2 & 3 UI changes

--- CoD MW2 Trainer/MW2 Trainer/Form1.Designer.cs
+++ CoD MW2 Trainer/MW2 Trainer/Form1.Designer.cs
@@ -51,19 +51,20 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(10, 10);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.Size = new System.Drawing.Size(97, 25);
             this.label1.TabIndex = 0;
             this.label1.Text = "Process:";
             // 
             // ProcessID
             // 
             this.ProcessID.AutoSize = true;
-            this.ProcessID.Location = new System.Drawing.Point(62, 11);
+            this.ProcessID.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ProcessID.Location = new System.Drawing.Point(108, 11);
             this.ProcessID.Name = "ProcessID";
-            this.ProcessID.Size = new System.Drawing.Size(57, 13);
+            this.ProcessID.Size = new System.Drawing.Size(103, 25);
             this.ProcessID.TabIndex = 1;
             this.ProcessID.Text = "Not Found";
             // 
@@ -74,9 +75,10 @@
             // godmode_checkbox
             // 
             this.godmode_checkbox.AutoSize = true;
-            this.godmode_checkbox.Location = new System.Drawing.Point(13, 49);
+            this.godmode_checkbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.godmode_checkbox.Location = new System.Drawing.Point(15, 87);
             this.godmode_checkbox.Name = "godmode_checkbox";
-            this.godmode_checkbox.Size = new System.Drawing.Size(76, 17);
+            this.godmode_checkbox.Size = new System.Drawing.Size(123, 29);
             this.godmode_checkbox.TabIndex = 2;
             this.godmode_checkbox.Text = "God Mode";
             this.godmode_checkbox.UseVisualStyleBackColor = true;
@@ -84,11 +86,11 @@
             // 
             // timescale_trackbar
             // 
-            this.timescale_trackbar.Location = new System.Drawing.Point(228, 26);
+            this.timescale_trackbar.Location = new System.Drawing.Point(593, 131);
             this.timescale_trackbar.Maximum = 30;
             this.timescale_trackbar.Name = "timescale_trackbar";
             this.timescale_trackbar.Orientation = System.Windows.Forms.Orientation.Vertical;
-            this.timescale_trackbar.Size = new System.Drawing.Size(45, 210);
+            this.timescale_trackbar.Size = new System.Drawing.Size(45, 396);
             this.timescale_trackbar.TabIndex = 3;
             this.timescale_trackbar.Value = 5;
             this.timescale_trackbar.Scroll += new System.EventHandler(this.trackBar1_Scroll);
@@ -96,19 +98,20 @@
             // timescale_Label
             // 
             this.timescale_Label.AutoSize = true;
-            this.timescale_Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.timescale_Label.Location = new System.Drawing.Point(212, 10);
+            this.timescale_Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timescale_Label.Location = new System.Drawing.Point(577, 88);
             this.timescale_Label.Name = "timescale_Label";
-            this.timescale_Label.Size = new System.Drawing.Size(70, 13);
+            this.timescale_Label.Size = new System.Drawing.Size(121, 25);
             this.timescale_Label.TabIndex = 4;
             this.timescale_Label.Text = "Time Scale";
             // 
             // infammo_checkbox
             // 
             this.infammo_checkbox.AutoSize = true;
-            this.infammo_checkbox.Location = new System.Drawing.Point(13, 69);
+            this.infammo_checkbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.infammo_checkbox.Location = new System.Drawing.Point(14, 122);
             this.infammo_checkbox.Name = "infammo_checkbox";
-            this.infammo_checkbox.Size = new System.Drawing.Size(89, 17);
+            this.infammo_checkbox.Size = new System.Drawing.Size(149, 29);
             this.infammo_checkbox.TabIndex = 5;
             this.infammo_checkbox.Text = "Infinite Ammo";
             this.infammo_checkbox.UseVisualStyleBackColor = true;
@@ -116,35 +119,36 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(10, 108);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 181);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(112, 13);
+            this.label2.Size = new System.Drawing.Size(193, 25);
             this.label2.TabIndex = 6;
             this.label2.Text = "F2 = No Clip Mode";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(10, 129);
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(14, 217);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(87, 13);
+            this.label3.Size = new System.Drawing.Size(150, 25);
             this.label3.TabIndex = 7;
             this.label3.Text = "No Clip Mode:";
             // 
             // noclip_toggle
             // 
             this.noclip_toggle.AutoSize = true;
-            this.noclip_toggle.Location = new System.Drawing.Point(95, 129);
+            this.noclip_toggle.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.noclip_toggle.Location = new System.Drawing.Point(170, 217);
             this.noclip_toggle.Name = "noclip_toggle";
-            this.noclip_toggle.Size = new System.Drawing.Size(48, 13);
+            this.noclip_toggle.Size = new System.Drawing.Size(88, 25);
             this.noclip_toggle.TabIndex = 8;
             this.noclip_toggle.Text = "Disabled";
             // 
             // resetBtn
             // 
-            this.resetBtn.Location = new System.Drawing.Point(272, 119);
+            this.resetBtn.Location = new System.Drawing.Point(637, 311);
             this.resetBtn.Name = "resetBtn";
             this.resetBtn.Size = new System.Drawing.Size(61, 23);
             this.resetBtn.TabIndex = 9;
@@ -156,7 +160,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(269, 31);
+            this.label4.Location = new System.Drawing.Point(634, 136);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(107, 13);
             this.label4.TabIndex = 10;
@@ -166,7 +170,7 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(269, 215);
+            this.label5.Location = new System.Drawing.Point(632, 505);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(109, 13);
             this.label5.TabIndex = 11;
@@ -174,7 +178,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(272, 49);
+            this.button1.Location = new System.Drawing.Point(637, 154);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(25, 23);
             this.button1.TabIndex = 12;
@@ -184,7 +188,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(272, 189);
+            this.button2.Location = new System.Drawing.Point(635, 479);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(25, 23);
             this.button2.TabIndex = 13;
@@ -197,8 +201,8 @@
             this.label6.AutoSize = true;
             this.label6.Cursor = System.Windows.Forms.Cursors.Hand;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.ForeColor = System.Drawing.Color.LightSeaGreen;
-            this.label6.Location = new System.Drawing.Point(7, 215);
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(559, 623);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(192, 13);
             this.label6.TabIndex = 14;
@@ -209,8 +213,8 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.Window;
-            this.ClientSize = new System.Drawing.Size(393, 245);
+            this.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.ClientSize = new System.Drawing.Size(763, 645);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
--- CoD MW3 Trainer/MW3 Trainer/Form1.Designer.cs
+++ CoD MW3 Trainer/MW3 Trainer/Form1.Designer.cs
@@ -49,19 +49,20 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(10, 10);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.Size = new System.Drawing.Size(97, 25);
             this.label1.TabIndex = 0;
             this.label1.Text = "Process:";
             // 
             // ProcessID
             // 
             this.ProcessID.AutoSize = true;
-            this.ProcessID.Location = new System.Drawing.Point(62, 11);
+            this.ProcessID.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ProcessID.Location = new System.Drawing.Point(103, 10);
             this.ProcessID.Name = "ProcessID";
-            this.ProcessID.Size = new System.Drawing.Size(57, 13);
+            this.ProcessID.Size = new System.Drawing.Size(103, 25);
             this.ProcessID.TabIndex = 1;
             this.ProcessID.Text = "Not Found";
             // 
@@ -72,9 +73,11 @@
             // godmode_checkbox
             // 
             this.godmode_checkbox.AutoSize = true;
-            this.godmode_checkbox.Location = new System.Drawing.Point(13, 49);
+            this.godmode_checkbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.godmode_checkbox.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.godmode_checkbox.Location = new System.Drawing.Point(17, 109);
             this.godmode_checkbox.Name = "godmode_checkbox";
-            this.godmode_checkbox.Size = new System.Drawing.Size(76, 17);
+            this.godmode_checkbox.Size = new System.Drawing.Size(123, 29);
             this.godmode_checkbox.TabIndex = 2;
             this.godmode_checkbox.Text = "God Mode";
             this.godmode_checkbox.UseVisualStyleBackColor = true;
@@ -82,11 +85,11 @@
             // 
             // timescale_trackbar
             // 
-            this.timescale_trackbar.Location = new System.Drawing.Point(228, 26);
+            this.timescale_trackbar.Location = new System.Drawing.Point(585, 122);
             this.timescale_trackbar.Maximum = 30;
             this.timescale_trackbar.Name = "timescale_trackbar";
             this.timescale_trackbar.Orientation = System.Windows.Forms.Orientation.Vertical;
-            this.timescale_trackbar.Size = new System.Drawing.Size(45, 210);
+            this.timescale_trackbar.Size = new System.Drawing.Size(45, 406);
             this.timescale_trackbar.TabIndex = 3;
             this.timescale_trackbar.Value = 5;
             this.timescale_trackbar.Scroll += new System.EventHandler(this.trackBar1_Scroll);
@@ -94,26 +97,28 @@
             // timescale_Label
             // 
             this.timescale_Label.AutoSize = true;
-            this.timescale_Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.timescale_Label.Location = new System.Drawing.Point(212, 10);
+            this.timescale_Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timescale_Label.Location = new System.Drawing.Point(580, 94);
             this.timescale_Label.Name = "timescale_Label";
-            this.timescale_Label.Size = new System.Drawing.Size(83, 13);
+            this.timescale_Label.Size = new System.Drawing.Size(141, 25);
             this.timescale_Label.TabIndex = 4;
             this.timescale_Label.Text = "Gravity Scale";
             // 
             // infammo_checkbox
             // 
             this.infammo_checkbox.AutoSize = true;
-            this.infammo_checkbox.Location = new System.Drawing.Point(13, 69);
+            this.infammo_checkbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.infammo_checkbox.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.infammo_checkbox.Location = new System.Drawing.Point(17, 157);
             this.infammo_checkbox.Name = "infammo_checkbox";
-            this.infammo_checkbox.Size = new System.Drawing.Size(89, 17);
+            this.infammo_checkbox.Size = new System.Drawing.Size(149, 29);
             this.infammo_checkbox.TabIndex = 5;
             this.infammo_checkbox.Text = "Infinite Ammo";
             this.infammo_checkbox.UseVisualStyleBackColor = true;
             // 
             // resetBtn
             // 
-            this.resetBtn.Location = new System.Drawing.Point(272, 119);
+            this.resetBtn.Location = new System.Drawing.Point(629, 312);
             this.resetBtn.Name = "resetBtn";
             this.resetBtn.Size = new System.Drawing.Size(61, 23);
             this.resetBtn.TabIndex = 9;
@@ -125,7 +130,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(269, 31);
+            this.label4.Location = new System.Drawing.Point(626, 128);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(111, 13);
             this.label4.TabIndex = 10;
@@ -135,7 +140,7 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(269, 215);
+            this.label5.Location = new System.Drawing.Point(624, 507);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(113, 13);
             this.label5.TabIndex = 11;
@@ -143,7 +148,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(272, 49);
+            this.button1.Location = new System.Drawing.Point(629, 152);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(25, 23);
             this.button1.TabIndex = 12;
@@ -153,7 +158,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(272, 189);
+            this.button2.Location = new System.Drawing.Point(629, 477);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(25, 23);
             this.button2.TabIndex = 13;
@@ -166,8 +171,8 @@
             this.label6.AutoSize = true;
             this.label6.Cursor = System.Windows.Forms.Cursors.Hand;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.ForeColor = System.Drawing.Color.LightSeaGreen;
-            this.label6.Location = new System.Drawing.Point(7, 215);
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(559, 623);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(192, 13);
             this.label6.TabIndex = 14;
@@ -177,7 +182,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(13, 119);
+            this.label2.Location = new System.Drawing.Point(14, 211);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(35, 13);
             this.label2.TabIndex = 15;
@@ -187,8 +192,8 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.Window;
-            this.ClientSize = new System.Drawing.Size(393, 245);
+            this.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.ClientSize = new System.Drawing.Size(763, 645);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.button2);
